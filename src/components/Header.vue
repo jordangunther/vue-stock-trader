6df8342684b5601cfed929f22c9eef5992169791
Jordan Gunther <jordankgunther@gmail.com>
2019-07-01 16:59:10 -0600

    added bootstrap nav and the stock/stocks components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,37 @@
 <template>
-    <h1>The Header component</h1>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+            </div>
+
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+                    <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="">End Day</a></li>
+                    <li class="dropdown">
+                        <a
+                                class="dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false">Save & Load <span class="caret"></span>
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="#">Save Data</a>
+                            <a href="#">Load Data</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+
 </template>
 
 <script>
