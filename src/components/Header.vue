b81a7509feefa7911e35465204d28aba5ac0e348
Jordan Gunther <jordankgunther@gmail.com>
2019-07-01 15:11:10 -0600

    created components and installed vue-router

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>The Header component</h1>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>The Stocks - Stock component</h1>
+</template>
+
+<script>
+    export default {
+        name: "Stock"
+    }
+</script>
+
+<style scoped>
+
+</style>
