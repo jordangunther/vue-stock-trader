5b8a258256089eddbc0d6dec515da3e81869ef17
Jordan Gunther <jordankgunther@gmail.com>
2019-07-02 13:33:52 -0600

    set up portfolio vue to call the stock portfolio getter and pass them into the displayed portfolio stocks

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
     </div>
 </template>
 
 <script>
-    export default {
+    import {mapGetters} from 'vuex';
+    import Stock from "../portfolio/Stock";
 
+    export default {
+        computed: {
+            ...mapGetters({
+                stocks: 'stockPortfolio'
+            })
+        },
+        components: {
+            appStock: Stock
+        }
     }
 </script>
 
